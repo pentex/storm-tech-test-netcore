86a3450b5a7807ad4b0a77779fb8c3d3d18eea14
Stephen <pentex_1@msn.com>
2020-07-02 22:00:04 +0100

    Task 6 - Added a checkbox that allows you to hide items that have been completed. I've used javascript to show and hide the items so that the page doesn't need to be reloaded each time there's a change.

--- Todo/wwwroot/js/site.js
+++ Todo/wwwroot/js/site.js
@@ -1,4 +1,13 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+$(() => {
+    $("#hide-completed-items-checkbox").change(function () {
+        if ($(this).is(":checked")) {
+            $(".is-done").hide();
+        }
+        else {
+            $(".is-done").show();
+        }
+    });
+});
